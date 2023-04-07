7a8c91d4b6c3ec5da7275dc3bdc2fd888a858295
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-04-07 10:02:55 +0530

    Removed catchAsync from reviewController

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,6 +1,4 @@
 const Review = require("../models/reviewModel");
-const catchAsync = require("../utils/catchAsync");
-const AppError = require("../utils/appError");
 const {
   deleteOne,
   updateOne,
