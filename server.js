a68259bcc98ea0a9887cbf8e791ce6ced71c8d9b
Dhruvjyoti Ray <dhrubojyoti21@gmail.com>
2023-04-22 10:44:35 +0530

    Added changes for vercel deployment

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
-const mongoose = require("mongoose");
-const app = require("./app");
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+const mongoose = require('mongoose');
+const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
-
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
   })
-  .then(() => console.log("DB connection successful!"));
-
+  .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`App running on port ${port}...`);
+  });
+}
+
+module.exports = app;
